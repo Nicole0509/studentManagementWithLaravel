ad0bd54f2c798207dfe565a73802d81fd44f6dfc
Nicole0509 <niashavril0509@gmail.com>
2025-06-27 18:29:16 +0200

    using for loops and if conditions in the view

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -4,5 +4,14 @@
         <h2>Name: {{$name}}</h2>
         {{-- <h2>Email: {{$email}}</h2> --}}
         <h2>ID: {{$id}}</h2>
+
+        @for ($i = 0;$i < 10;$i++)
+            <p>Item {{$i}}</p>
+            @if($i%2 == 0)
+                <p>Even number</p>
+            @else
+                <p>Odd number</p>   
+            @endif        
+        @endfor
     </body>
 </html>
