e69d4b1d892f007f8826574b59f90d4394f01b96
Nicole0509 <niashavril0509@gmail.com>
2025-07-02 14:04:24 +0200

    invoke and resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
+use App\Http\Controllers\SecondTestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('about-us/',function(){
-//     return view('aboutus');
-// });
 
 Route::view('contact-us/', 'contactus');
 
@@ -18,3 +16,7 @@ Route::controller(StudentController::class)->group(function()
     Route::get('students', 'index');
     Route::get('about-us/', 'aboutUs');
 });
+
+Route::get('invoke',TestController::class);
+
+Route::resource('second-test',SecondTestController::class);
