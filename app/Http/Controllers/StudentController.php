8bbcf16d5f15a9f7309ae9a45f4b0397cf4d27fe
Nicole0509 <niashavril0509@gmail.com>
2025-07-05 10:25:00 +0200

    using query builder to store and fetch records from/to the DB

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,28 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
-    protected $name;
-    public function __construct()
-    {
-        $this->name = 'My name';
-    }
+   public function addData(){
 
-    public function index(){
-        return 'hello from the controller';
-    }
+    // Insert a single student record into the 'student' table
+    // DB::table('student')->insert([
+    //     'name' => 'tester',
+    //     'email' => 'tester@gmail.com',
+    //     'age' => 20,
+    //     'date_of_birth' => '2003-01-01',       
+    //     'gender' => 'M',       
+    // ]);
 
-    public function aboutUs(){
-        // $name = $this->priveteMethod();
+    // Insert multiple student records into the 'student' table
 
-        // return $name;
-        return $this->name;
-    }
-    
-    private function priveteMethod() {
-        // This is a private method that can be used within this controller
-        return 'This is a private method';
-    }
+    DB::table('student')->insert([
+        ['name' => 'gffgfgf',
+        'email' => 'testfgfgfger@gmail.com',
+        'age' => 80,
+        'date_of_birth' => '2003-01-01',       
+        'gender' => 'M', 
+        ],
+        ['name' => 't687778gg',
+        'email' => 'gytrtytry@gmail.com',
+        'age' => 6,
+        'date_of_birth' => '2003-01-01',       
+        'gender' => 'F', 
+        ],
+        ['name' => 'tester',
+        'email' => 'tester8888@gmail.com',
+        'age' => 78,
+        'date_of_birth' => '2793-01-01',       
+        'gender' => 'M', 
+        ],
+    ]);
+
+    return 'Student added successfully';
+   }
+
+   public function getData(){
+    $items = DB::table('student')
+    ->where('id', '=', 2)
+    ->orWhere('name', 'like', '%test%')
+    ->orderBy('id', 'desc')
+    ->select('id', 'name',)
+    ->get();
+    return $items;
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,15 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TeacherController;
+use App\Models\Student;
 use App\Models\Teacher;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('teachers',function(){
-//     return Teacher::all();
-// });
+Route::get('add-data',[StudentController::class, 'addData']);
 
-Route::get('teacher',[TeacherController::class,'index']);
-Route::get('add-teacher',[TeacherController::class,'add']);
-Route::get('show-teacher/{id}',[TeacherController::class,'show']);
-Route::get('update-teacher/{id}',[TeacherController::class,'update']);
-Route::get('delete-teacher/{id}',[TeacherController::class,'delete']);
+Route::get('get-data', [StudentController::class,'getData']);
